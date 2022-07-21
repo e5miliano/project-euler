bbb8b8d1069e566b8787df3596d1bf08215e7355
santiagoacasas <89913087+santiagoacasas@users.noreply.github.com>
2022-07-21 14:14:41 +0200

    solved problem 4

--- 4.hs
+++ 4.hs
@@ -4,12 +4,15 @@
 
 -- Find the largest palindrome made from the product of two 3-digit numbers.
 
+import Data.List (tails, nub, sort, group)
+
 main =
-    putStrLn "" 
-  
+    print ( last (filter isPalindrome (map product (nub (map sort (sequence [[100..1000],[100..1000]] )))) ) )
 
-isPalindrome x = x == reverse x
-
-
-reverseInt x | x < 0     = 0 - (read . reverse . tail . show $ x)
+reverseInt x | x < 0     = negate (read . reverse . tail . show $ x)
              | otherwise = read . reverse . show $ x
+
+isPalindrome x = x == reverseInt x
+
+generatePalindromes n = take n (filter isPalindrome [2..])
+
