3a5ec9d9f0d58a46000c5b54e8df8e1801f44d1a
santiagoacasas <89913087+santiagoacasas@users.noreply.github.com>
2022-07-21 12:53:08 +0200

    solved problem 10

--- 10.hs
+++ 10.hs
@@ -1,9 +1,13 @@
+-- Problem 10
+
+-- Find the sum of all the primes below two million.
+
+main =
+    print (sum (primeGenerator 2000000))
+
+isPrime n = not (any (\x -> n `mod` x == 0) [2..n-1])
+
+primeGenerator n = filter isPrime [2..n]
 
 
-
-
-
-filterPrime [] = 0
-
-filterPrime (x:xs) = p : filterPrime [x | x <- xs, x `mod` w /= 0]
 
